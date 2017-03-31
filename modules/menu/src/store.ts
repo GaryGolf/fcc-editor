fbcd30c9e66c0e3da6c62524308280240b055699
GaryGolf <garygolf@mail.ru>
2017-03-31 09:27:40 +0300

    DGSAPP-363-taginput remove epic (observables)

--- modules/menu/src/store.ts
+++ modules/menu/src/store.ts
@@ -1,20 +1,15 @@
 import { createStore, applyMiddleware, Store, Middleware } from 'redux'
-import {createEpicMiddleware} from 'redux-observable'
-import * as createLogger from 'redux-logger'
+import {createLogger} from 'redux-logger'
 import ReduxThunk from 'redux-thunk'
 import * as ReduxPromise from 'redux-promise'
 import {routerMiddleware} from 'react-router-redux'
 import rootReducer, { RootState } from './reducers'
-// import rootEpic from './epics'
-
 
 export default function configureStore(history, initialState?: RootState): Store<RootState> {
   const create = window.devToolsExtension && !PRODUCTION
-    ? window.devToolsExtension()(createStore)
-    : createStore
+    ? window.devToolsExtension()(createStore) : createStore
 
-  // const epicMiddleware = createEpicMiddleware(rootEpic)
-  const middleware: Middleware[] =[ReduxThunk, ReduxPromise, routerMiddleware(history)] // epicMiddleware] 
+  const middleware: Middleware[] =[ReduxThunk, ReduxPromise, routerMiddleware(history)]
   if(!PRODUCTION) middleware.push(createLogger({collapsed: true}))  
   const createStoreWithMiddleware = applyMiddleware(...middleware)(create)
 
@@ -24,10 +19,6 @@ export default function configureStore(history, initialState?: RootState): Store
     module.hot.accept('./reducers', () => {
         store.replaceReducer(require('./reducers'))
     })
-    // module.hot.accept('./epics', () => {
-    //   const rootEpic = require('./epics').default
-    //   epicMiddleware.replaceEpic(rootEpic)
-    // })
   }
 
   return store
