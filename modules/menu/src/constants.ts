4d11552e2e2f8beed6fdfae6a23551fcf63c4139
GaryGolf <garygolf@mail.ru>
2017-05-10 11:59:10 +0300

    DGSAPP-525-menu

--- modules/menu/src/constants.ts
+++ modules/menu/src/constants.ts
@@ -2,8 +2,7 @@
 function getDomain(){
     const proxy = PRODUCTION ? '' : 'localhost:1337/'
     const tenant = document.querySelector('meta[name=tenant-domain]')['content']
-    const domain = ENV_DOMAIN
-    return `http://${proxy}${tenant}.${domain}`
+    return `http://${proxy}${tenant}.${ENV_DOMAIN}`
 }
 
 export const DOMAIN = getDomain()
