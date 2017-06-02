9477eef908441748a1c6f0f9a1839bf1e7b09d36
GaryGolf <garygolf@mail.ru>
2017-06-02 15:04:34 +0300

    DGS-1083-plan modal

--- modules/plan/src/components/products-plan/new-item-modal.tsx
+++ modules/plan/src/components/products-plan/new-item-modal.tsx
@@ -1,15 +1,70 @@
 import * as React from 'react'
 import * as CONST from '../../constants'
+import * as Actions from '../../actions'
 import * as styles from './new-item-modal.css'
+import { bindActionCreators } from 'redux'
+const {connect} = require('react-redux')
 
 interface Props {
     onClose():void
     visible: boolean
+    products?: Product[]
 }
 
-export default class NewItemModal extends React.Component <Props, null> {
+interface State {
+    id: string
+    qty: number
+}
+
+@connect(
+    state => ({
+        // salesplan: state.salesplan as SalesPlan,
+        // planitems: state.planitems as PlanItem[],
+        products: state.products as Product[]
+        // salesreport: state.salesreport as SalesReport[]
+    }),
+    dispatch => ({
+        actions: {
+            salesplan: bindActionCreators(Actions.salesplan as any, dispatch),
+            planitems: bindActionCreators(Actions.planitems as any, dispatch),
+            products: bindActionCreators(Actions.products as any, dispatch),
+            salesreport: bindActionCreators(Actions.salesreport as any, dispatch)
+        } 
+    })
+)
+export default class NewItemModal extends React.Component <Props, State> {
+
+    constructor(props:Props){
+        super(props)
+        this.state={
+            id: null,
+            qty: 0
+        }
+    }
+
+    componentWillReceiveProps(nextProps){
+        if(!this.state.id && nextProps.products.length) this.setState({id: nextProps.products[0].id})
+    }
+    productChangeHandler(e){
+        this.setState({id: e.target.value})
+    }
+    quantityChangeHandler(e){
+        // check input here
+        this.setState({qty: e.target.value})
+    }
     render(){
-        if(!this.props.visible) return null
+        const {visible, products} = this.props
+        const {id, qty} = this.state
+        if(!visible || !products) return null
+        // console.log(products[0])
+        const options = products.map(item=>(<option key={item.id} value={item.id}>{item.name}</option>))
+        const prod = products.find(item => item.id == id)
+        console.log(prod)
+        const price = !prod ? '' : prod.price.toString()
+        const cost_price = !prod ? '' : prod.cost_price.toString()
+        const amount = !qty ? '' : (prod.price*qty).toString()
+        const profit = !qty ? '' : (prod.price*qty - prod.cost_price*qty).toString()
+
         return (
          <div className={styles.overlay} onClick={this.props.onClose}>
              <div className="modal-dialog" 
@@ -24,10 +79,50 @@ export default class NewItemModal extends React.Component <Props, null> {
                             aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
-                        <h2 className="modal-title">{CONST.DOMAIN}</h2>
+                        <h2 className="modal-title">Add new product</h2>
                     </div>
                     <div className="modal-body">
-                        <p>Hello</p>
+                        <div className="form-group">
+                            <label>Product</label>
+                            <select className="form-control"
+                                onChange={this.productChangeHandler.bind(this)}>
+                                {options}
+                            </select>
+                        </div>
+                        <div className="form-group">
+                            <label>Qty</label>
+                            <input className="form-control"
+                                type="number"
+                                onChange={this.quantityChangeHandler.bind(this)}/>
+                        </div>
+                        <div className="form-group">
+                            <label>price</label>
+                            <input className="form-control"
+                                value={price}
+                                disabled
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label>cost</label>
+                            <input className="form-control"
+                                value={cost_price}
+                                disabled
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label>amount</label>
+                            <input className="form-control"
+                                value={amount}
+                                disabled
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label>Profit</label>
+                            <input className="form-control"
+                                value={profit}
+                                disabled
+                            />
+                        </div>
                     </div>
                     <div className="modal-footer">
                         <button type="button" 
--- modules/plan/src/components/products-plan/products-plan.tsx
+++ modules/plan/src/components/products-plan/products-plan.tsx
@@ -1,17 +1,28 @@
 import * as React from 'react'
 import * as styles from './products-plan.css'
+import NewItemModal from './new-item-modal'
 
 interface Props {}
-interface State {}
+interface State {
+    showNewItemModal: boolean
+}
 
 export default class ProductsPlan extends React.Component<Props, State> {
 
+    state={
+        showNewItemModal: false
+    }
     addNewItemHandler(){
         console.log('add new item')
+        this.setState({showNewItemModal: true})
     }
     render(){
         return (
             <div className={styles.container}>
+                <NewItemModal
+                    onClose={()=>this.setState({showNewItemModal: false})}
+                    visible={this.state.showNewItemModal}
+                />
                 <button className="btn btn-default"
                     onClick={this.addNewItemHandler.bind(this)}>
                     Add
